91d4d5060e2f2ee1d7c74a1ec41ef7995fa65916
ToyLess78 <121128928+ToyLess78@users.noreply.github.com>
2024-03-09 19:47:47 +0200

    + add spinner

---
+++ src/components/Spinner.tsx
@@ -0,0 +1,15 @@
+import Box from '@mui/material/Box';
+
+const Spinner = () => {
+    return (
+    <Box sx={{
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        width: '15%',
+        margin: '-5% 0 0 -5%' }}>
+        <object type='image/svg+xml' data={'../../public/spinner.svg'}></object>
+    </Box>
+)};
+
+export default Spinner;
