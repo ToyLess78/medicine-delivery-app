f7c19b1e3f848f597706829972028f50c09a0a51
ToyLess78 <121128928+ToyLess78@users.noreply.github.com>
2024-03-10 17:23:05 +0200

    + add history loader

--- src/pages/History.tsx
+++ src/pages/History.tsx
@@ -5,6 +5,7 @@ import {useSelector} from 'react-redux';
 import {selectCustomer} from '../store/slices/customer.slice.ts';
 import {useGetOrderQuery} from '../store/api/order.api.ts';
 import HistoryCard from '../components/HistoryCard.tsx';
+import Spinner from '../components/Spinner.tsx';
 
 const History = () => {
     const {data: drugs, error, isLoading} = useGetPharmaciesQuery('');
@@ -14,52 +15,55 @@ const History = () => {
 
     if (error || ordersError) console.log(error || ordersError);
 
-    console.log(orders, customer, drugs)
     console.log(ordersIsLoading, isLoading)
 
     return (
-        <Box sx={{
-            flexGrow: 1,
-            height: '82vh',
-            display: 'flex',
-            border: (theme) => `1px solid ${theme.palette.divider}`,
-            borderRadius: 1.5,
-            flexWrap: 'wrap',
-            justifyContent: 'center',
-            p: 3,
-            overflow: 'auto'
-        }}>
-            <Grid container spacing={3}>
-                {orders?.map(i => {
-                    const order = i.order;
-                    const uId = i?.id;
-                    const totalPrice = i.totalPrice;
-                    if (!order?.length || !totalPrice) {
-                        return;
-                    }
-                    return <HistoryCard
-                        key={uId}
-                        totalPrise={totalPrice}
-                        cardList={
-                            order?.map(i => {
-                                const drug = drugs?.find(d => d.id === i.id);
-                                if (!drug) {
-                                    return;
-                                }
-                                return <DrugCartCard
-                                    price={drug.price}
-                                    genericName={drug.genericName}
-                                    isAction={false}
-                                    width={47}
-                                    id={drug.id}
-                                    key={drug.id}
-                                />;
-                            })
+        <>
+            {(ordersIsLoading || isLoading) && <Spinner />}
+            {(!ordersIsLoading || !isLoading) &&
+                <Box sx={{
+                flexGrow: 1,
+                height: '82vh',
+                display: 'flex',
+                border: (theme) => `1px solid ${theme.palette.divider}`,
+                borderRadius: 1.5,
+                flexWrap: 'wrap',
+                justifyContent: 'center',
+                p: 3,
+                overflow: 'auto'
+            }}>
+                <Grid container spacing={3}>
+                    {orders?.map(i => {
+                        const order = i.order;
+                        const uId = i?.id;
+                        const totalPrice = i.totalPrice;
+                        if (!order?.length || !totalPrice) {
+                            return;
                         }
-                    />
-                })}
-            </Grid>
-        </Box>
+                        return <HistoryCard
+                            key={uId}
+                            totalPrise={totalPrice}
+                            cardList={
+                                order?.map(i => {
+                                    const drug = drugs?.find(d => d.id === i.id);
+                                    if (!drug) {
+                                        return;
+                                    }
+                                    return <DrugCartCard
+                                        price={drug.price}
+                                        genericName={drug.genericName}
+                                        isAction={false}
+                                        width={47}
+                                        id={drug.id}
+                                        key={drug.id}
+                                    />;
+                                })
+                            }
+                        />
+                    })}
+                </Grid>
+            </Box>}
+        </>
 
     );
 }
